bca778427d2696cbd90f859edef33f1ef8bc710c
larads <dasilval.mariana@gmail.com>
2024-08-20 15:41:00 -0400

    ✨ feat: Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,3 @@
-import http from 'node:http'
-// - Criar usuários
-// - Listagem usuários
-// - Edição de usuários
-// - Remoção de usuários
 // - HTTP
 //   - Método HTTP
 //   - URL
@@ -14,15 +9,26 @@ import http from 'node:http'
 // DELETE => Deletar um recurso do back-end
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
-
 // Stateful - Stateless
-
 // Cabeçalhos (Requisição/resposta) => Metadados
+// HTTP Status Code
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
 
     if (method === 'GET' && url === '/users') {
         return res
@@ -31,15 +37,16 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
         users.push({
             id: 1,
-            name: 'Mariana Lara',
-            email: 'mariana@example.com',
+            name,
+            email,
         })
 
         return res.writeHead(201).end()
     }
-
     return res.writeHead(404).end()
 })
 server.listen(3333)
