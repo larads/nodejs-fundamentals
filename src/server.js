6cc8551a7fb57ef0fd29f26a681cc27acbadabf7
larads <dasilval.mariana@gmail.com>
2024-08-20 15:55:21 -0400

    ✨ feat: Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,49 +1,34 @@
-// - HTTP
-//   - Método HTTP
-//   - URL
-// GET, POST, PUT, PATCH, DELETE
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-// GET /users => Buscando usuários no banc-end
-// POST /users => Criar um usuário no back-end
-// Stateful - Stateless
-// Cabeçalhos (Requisição/resposta) => Metadados
-// HTTP Status Code
-import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
-import { json } from './middlewares/json.js'
+// Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
+// Route Parameters: Identificação de recurso
+// Request Body: Envio de informações de um formulário (HTTPs)
 
-const database = new Database()
+//http://localhost:3333/users?userId=1&name=Diego
+
+// GET http://localhost:3333/users/1
+// DELETE http: //localhost:3333/users/1
+
+// POST http://localhost:3333/users
+
+// Edição e remoção
+
+import http from 'node:http'
+
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users') {
-        const users = database.select('users')
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-        return res.end(JSON.stringify(users))
-    }
-
-    if (method === 'POST' && url === '/users') {
-        const { name, email } = req.body
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-        }
-
-        database.insert('users', user)
-
-        return res.writeHead(201).end()
+    if (route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
 })
-
 server.listen(3333)
