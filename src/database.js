06fbf26897a107d213e96a79c40622f924055f5f
larads <dasilval.mariana@gmail.com>
2024-08-20 16:01:51 -0400

    ✨ feat: Remoção de registros

--- src/database.js
+++ src/database.js
@@ -18,22 +18,27 @@ export class Database {
     #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
-
     select(table) {
         const data = this.#database[table] ?? []
-
         return data
     }
-
     insert(table, data) {
         if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
         } else {
             this.#database[table] = [data]
         }
-
         this.#persist()
 
         return data
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
     }
 }
