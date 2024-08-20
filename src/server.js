1044f16936770656525106a492eda2890d248856
larads <dasilval.mariana@gmail.com>
2024-08-20 15:45:22 -0400

    ✨ feat: Criando middleware de JSON

--- src/server.js
+++ src/server.js
@@ -12,41 +12,30 @@
 // Stateful - Stateless
 // Cabeçalhos (Requisição/resposta) => Metadados
 // HTTP Status Code
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
-    const buffers = []
-
-    for await (const chunk of req) {
-        buffers.push(chunk)
-    }
-
-    try {
-        req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-    }
+    await json(req, res)
 
     if (method === 'GET' && url === '/users') {
         return res
-            .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
         const { name, email } = req.body
-
         users.push({
             id: 1,
             name,
             email,
         })
-
         return res.writeHead(201).end()
     }
     return res.writeHead(404).end()
 })
+
 server.listen(3333)
