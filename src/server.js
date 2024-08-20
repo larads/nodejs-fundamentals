238adc6ba6a9a1f57310a8c3ff6eebb7a3568d6c
larads <dasilval.mariana@gmail.com>
2024-08-20 15:50:54 -0400

    ✨ feat: db

--- src/server.js
+++ src/server.js
@@ -12,9 +12,12 @@
 // Stateful - Stateless
 // Cabeçalhos (Requisição/resposta) => Metadados
 // HTTP Status Code
+import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -22,19 +25,24 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     if (method === 'GET' && url === '/users') {
-        return res
-            .end(JSON.stringify(users))
+        const users = database.select('users')
+
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
         const { name, email } = req.body
-        users.push({
-            id: 1,
+        const user = {
+            id: randomUUID(),
             name,
             email,
-        })
+        }
+
+        database.insert('users', user)
+
         return res.writeHead(201).end()
     }
+
     return res.writeHead(404).end()
 })
 
